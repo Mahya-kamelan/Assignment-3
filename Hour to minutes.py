557a81ccc79cda80df4be5be6fefd5a525b6753a
Mahya-kamelan <110662014+Mahya-kamelan@users.noreply.github.com>
2022-08-26 00:04:24 +0430

    Update Hour to minutes.py

--- Hour to minutes.py
+++ Hour to minutes.py
@@ -1,20 +1,20 @@
-s=0
+sh=0
 m=0
 h=0
 
 while True:
-    x=input("meghyas mored nazar ra vared konid( h ya s) ")
+    x=input("Enter the desired scale( h ya s) ")
     if x=="s":
-        t=int(input("zaman ra bar hasb sanieh vared konid "))
+        t=int(input("Enter the time in seconds "))
         s=t%60
         h=t//3600
         m=t//60-h*60
-        print("time: ",h,"saat",m,"daghigheh",s,"sanieh ")
+        print("time: ",h,"hour",m,"minutes",s,"second ")
         continue
     elif x=="h":
-        h=int(input("saat ravard konid "))
-        m=int(input("min ra vared konid "))
-        s=int(input("sanieh ra vared konid "))
+        h=int(input("Enter the hour "))
+        m=int(input("Enter the minute "))
+        s=int(input("Enter seconds "))
         time=(h*60*60)+(m*60)+(s)
-        print("zaman  bar hasb sanine :",time)
+        print("Time in seconds :",time)
         continue
