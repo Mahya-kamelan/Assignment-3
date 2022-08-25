a16b267ffd8a70fd3d3bf6457e08d823de266aa3
Mahya-kamelan <110662014+Mahya-kamelan@users.noreply.github.com>
2022-08-25 23:58:50 +0430

    Update Game.py

--- Game.py
+++ Game.py
@@ -5,27 +5,27 @@ x=100
 a=0
 x
 
-print("adad mored nazar ra dar bazeh 0-100 b khater bsparid v enter ra bzind")
+print("select a number between 0 to 99")
 
 while True:
      
-    adad=random.randint(a,x)
-    print("AYA",adad,"adad mored nzr shomast?")
-    y=int(input("1: adad bozorgtar ast \n"
+    number=random.randint(a,x)
+    print("is",number,"The numbers you want?")
+    y=int(input("1: number is bigger \n"
     " 2 adad dorost ast \n"
-    " 3 adad kuchek tar ast \n"))
+    " 3 number is lower \n"))
     c+=1
     if y==1:
-        a= adad
+        a= number
         continue
     elif y==2:
         break
     elif y==3:
-        x=adad
+        x=number
         continue
     else:
-        print("adad dorost ra az menu entekhab konid")
+        print("Choose the correct numbers from the menu")
         a=0
         x=100
         continue
-print("hooora adad mored naz shoma :", adad ,"ba",c ,"hads anjam shodeh")
+print("hooora The numbers you want :", number ,"with",c ,"Guess done")
